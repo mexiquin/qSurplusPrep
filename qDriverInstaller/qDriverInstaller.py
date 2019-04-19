75f839c93a70cfd5faae50729f66d846e400f493
Tech Guy <qjj2@nau.edu>
2019-04-19 12:24:50 -0700

    Implements simple functionality of the golang version
    - New welcom screen
    - written in python
    - new stable (similar to the version written in golang)

--- qDriverInstaller/qDriverInstaller.py
+++ qDriverInstaller/qDriverInstaller.py
@@ -23,17 +23,39 @@ import os
 import sys
 import glob
 import requests
+import subprocess
+import pyfiglet
+import cowsay
 
 # constant for finding the scripts dir with all dependencies
 # as well as constant for the desktop directory of any given machine
 scriptsDir =  os.path.join(os.getcwd(), 'Scripts')
 desktopDir = os.path.join(os.environ["HOMEPATH"], "Desktop")
 
+# Make sure to update this URL every time that windows 10 releases a major update
+mediaCreationToolURL = "https://software-download.microsoft.com/download/pr/MediaCreationTool1809.exe"
+
 # Main access point of the program. Runs all functions in order they need to be ran
 def main():
-    # Testing out functions
-    usrIn = input("Put url in here for testing:\n")
-    downloadToDir(usrIn, desktopDir)
+    # Simple main functionality to test if the build with pyInstaller will work
+
+    # Welcome interface
+    #printInterface()
+    printCowsay("Quinton's Driver Installer: Version 2.0")
+
+    # take input for what install is wanted
+    installChoice = input("(1) Install Network\n(2) Install Dell Command Update\n(ENTER) Both\n")
+
+    if "1" in installChoice:
+        installNetwork()
+    elif "2" in installChoice:
+        installDell()
+    else:
+        installNetwork()
+        installDell()
+
+    # Download Media Creation Tool to Desktop\
+    downloadToDir(mediaCreationToolURL, desktopDir)
         
 
 # Searches direcory for file that contains given keywords
@@ -41,13 +63,13 @@ def findExecutable(keyword):
 	for name in glob.glob('Scripts/%s.exe' % keyword):
 		return name
 
-# Install Dell Command update for later manual use
+# Install Dell Command update for later manual use (Works!)
 def installDell():
-	pass
+	subprocess.run([findExecutable("DCU"), "/s"])
 
-# Install network drivers from SDI Tool.
+# Install network drivers from SDI Tool. (Works!)
 def installNetwork():
-	pass
+	subprocess.run([findExecutable("SDI_x64"), "-autoinstall", "-autoclose", "-showconsole", "-nogui"])
 
 # Download a file from a given URL to the specified directory (Works!)
 def downloadToDir(url, outDir):
@@ -57,6 +79,17 @@ def downloadToDir(url, outDir):
 
         file = open(directory, "wb")
         file.write(requestor.content)
+
+# Installs the drivers utilizing ITS's driver library
+def installNAUDrivers():
+    pass
+
+def printInterface():
+    welcomeBanner = pyfiglet.figlet_format("Quinton's\nDriver\nInstaller")
+    print(welcomeBanner)
+
+def printCowsay(input):
+    cowsay.cow(input)
 
 # Finds the name of the file based on the url name (Works!)
 def fileNameFromURL(url):
